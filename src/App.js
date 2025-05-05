b2e45151ad8562412499f95e8894c1dcb42b0343
Abdullahmaghsoudi <abdullahmaghsoudi@gmail.com>
2025-05-05 21:41:57 +0330

    add file(App)

--- src/App.js
+++ src/App.js
@@ -2,9 +2,29 @@ import './App.css'
 
 const App = () => {
   return (
-    <h1 className='title'>
-      Pokemon Search
-    </h1>
+    <div
+      style={{
+        margin: 'auto',
+        width: '800px',
+        paddingTop: '1rem',
+      }}
+    >
+      <h1 className='title'>Pokemon Search</h1>
+      <table width='100%'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Bulbasaur</td>
+            <td>Grass, Poison</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
