8db0040d1c35b7b8d2837c9659ffdbaf6aebbfdb
Abdullahmaghsoudi <abdullahmaghsoudi@gmail.com>
2025-05-07 19:51:11 +0330

    add PropType

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import { useState } from 'react'
+
 import pokemon from './pokemon.json'
+import PropTypes from 'prop-types'
 
 import './App.css'
 
@@ -12,7 +14,18 @@ const PokemonRow = ({ pokemon }) => {
   )
 }
 
+PokemonRow.propType = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string,
+    }),
+    type: PropTypes.arrayOf(PropTypes.string),
+  }),
+}
+
 const App = () => {
+  const [filter, setFilter] = useState('')
+
   return (
     <div
       style={{
@@ -22,19 +35,35 @@ const App = () => {
       }}
     >
       <h1 className='title'>Pokemon Search</h1>
-      <table width='100%'>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          {pokemon.slice(0, 17).map((pokemon) => {
-            return <PokemonRow key={pokemon.id} pokemon={pokemon} />
-          })}
-        </tbody>
-      </table>
+      <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '70% 30%',
+          ColumnGap: '1rem',
+        }}
+      >
+        <table width='100%'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pokemon
+              .filter((pokemon) =>
+                pokemon.name.english
+                  .toLocaleLowerCase()
+                  .includes(filter.toLocaleLowerCase())
+              )
+              .slice(0, 17)
+              .map((pokemon) => {
+                return <PokemonRow key={pokemon.id} pokemon={pokemon} />
+              })}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
